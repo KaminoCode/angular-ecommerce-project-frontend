094e8a5dfbbe6d0473fff5ad221ce5305518b3b2
ozark-dev <jason@ozarkdev.com>
2023-05-03 22:33:15 -0500

    Added pagination support for max size and boundary links

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -92,4 +92,10 @@ export class ProductListComponent implements OnInit {
         this.theTotalElements = data.page.totalElements;
       });
   }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
