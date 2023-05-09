79acefffb089ad620c05c45c896e5bd765d9e269
ozark-dev <jason@ozarkdev.com>
2023-05-08 21:42:29 -0500

    Upated CheckoutComponent to retrieve months and years from KaminoCodeForm Service

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { KaminoCodeFormService } from 'src/app/services/kamino-code-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,7 +13,13 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {}
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private kaminoCodeFormService: KaminoCodeFormService
+  ) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -43,6 +50,23 @@ export class CheckoutComponent implements OnInit {
         expirationMonth: [''],
         expirationYear: [''],
       }),
+    });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('startMonth:' + startMonth);
+
+    this.kaminoCodeFormService
+      .getCreditCardMonths(startMonth)
+      .subscribe((data) => {
+        console.log('Retrieved credit card months: ' + JSON.stringify(data));
+        this.creditCardMonths = data;
+      });
+
+    // populate credit card years
+    this.kaminoCodeFormService.getCreditCardYears().subscribe((data) => {
+      console.log('Retrieved credit card years: ' + JSON.stringify(data));
+      this.creditCardYears = data;
     });
   }
 
