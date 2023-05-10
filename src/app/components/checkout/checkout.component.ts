4efc6841ab4959ff1e76f579dd85add41e3628df
ozark-dev <jason@ozarkdev.com>
2023-05-10 14:35:04 -0500

    Added Getter methods to checkout.component.ts for FormControls

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -97,6 +97,18 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
   copyShippingAddressToBillingAddress(event) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress'].setValue(
