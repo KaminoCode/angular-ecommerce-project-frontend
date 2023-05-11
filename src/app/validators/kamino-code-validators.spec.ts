4fb78d99e98beb03f21caf4ecc3bb1853e0d7977
ozark-dev <jason@ozarkdev.com>
2023-05-10 20:47:52 -0500

    Generated new class validators/KaminoCodeValidators

---
+++ src/app/validators/kamino-code-validators.spec.ts
@@ -0,0 +1,7 @@
+import { KaminoCodeValidators } from './kamino-code-validators';
+
+describe('KaminoCodeValidators', () => {
+  it('should create an instance', () => {
+    expect(new KaminoCodeValidators()).toBeTruthy();
+  });
+});
